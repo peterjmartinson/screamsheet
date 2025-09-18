cd2090d4468333be7fd78fbf3bb8353fe77454c6
Peter Martinson <you@example.com>
2025-09-17 23:13:35 -0400

    add shell runner

---
+++ screamsheet.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+LOG_FILE="/home/peter/Code/screamsheet/logfiles/screamsheet_log_$(date +\%Y\%m\%d).txt"
+/home/peter/Code/screamsheet/env/bin/python /home/peter/Code/screamsheet/src/mlb_screamsheet.py >> "$LOG_FILE" 2>&1
+lp -o sides=two-sided-long-edge /home/peter/Code/screamsheet/Files/MLB_Scores_$(date +\%Y\%m\%d).pdf >> "$LOG_FILE" 2>&1

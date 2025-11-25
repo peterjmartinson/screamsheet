29d1ecc49901f52b43a8acd9c3608f63e6226a69
Peter Martinson <peter.j.martinson@gmail.com>
2025-11-24 23:29:41 -0500

    hopefully run the whole thing

--- screamsheet.sh
+++ screamsheet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-LOG_FILE="/home/peter/Code/screamsheet/logfiles/screamsheet_log_$(date +\%Y\%m\%d).txt"
-/home/peter/Code/screamsheet/env/bin/python /home/peter/Code/screamsheet/src/screamsheet.py >> "$LOG_FILE" 2>&1
+mkdir -p /home/peter/Code/screamsheet_live/Files /home/peter/Code/screamsheet_live/logfiles
+LOG_FILE="/home/peter/Code/screamsheet_live/logfiles/screamsheet_log_$(date +\%Y\%m\%d).txt"
+/home/peter/Code/screamsheet_live/env/bin/python /home/peter/Code/screamsheet_live/src/screamsheet.py >> "$LOG_FILE" 2>&1
 # lp -o sides=two-sided-long-edge /home/peter/Code/screamsheet/Files/MLB_Scores_$(date +\%Y\%m\%d).pdf >> "$LOG_FILE" 2>&1
 lp -o sides=two-sided-long-edge /home/peter/Code/screamsheet/Files/NFL_Scores_$(date +\%Y\%m\%d).pdf >> "$LOG_FILE" 2>&1
 lp -o sides=two-sided-long-edge /home/peter/Code/screamsheet/Files/NHL_Scores_$(date +\%Y\%m\%d).pdf >> "$LOG_FILE" 2>&1
